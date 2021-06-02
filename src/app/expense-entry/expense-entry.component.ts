6a55f52a81eafe561c25e8a56d32809eb4926444
DRJJ007 <javad.jahangiri.niopdc@gmail.com>
2021-06-02 21:29:36 +0430

    Angular 8 - Data Binding

--- src/app/expense-entry/expense-entry.component.ts
+++ src/app/expense-entry/expense-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseEntry } from '../expense-entry';
 
 @Component({
   selector: 'app-expense-entry',
@@ -8,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class ExpenseEntryComponent implements OnInit {
 
   title: string;
+    expenseEntry: ExpenseEntry
   constructor() { }
 
   ngOnInit(): void {
-    this.title = "Expense Entry"
+    this.title = "Expense Entry";
+    this.expenseEntry = {
+         id: 1,
+         item: "Pizza",
+         amount: 21,
+         category: "Food",
+         location: "Zomato",
+         spendOn: new Date(2020, 6, 1, 10, 10, 10), createdOn: new Date(2020, 6, 1, 10, 10, 10),
+      };
   }
 
 }
